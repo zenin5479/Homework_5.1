eae03549395e1a9027d1098a3fb87ed16d9cbc0e
zenin5479 <zenin5479@gmail.com>
2025-07-07 21:12:54 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework_5._1
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
