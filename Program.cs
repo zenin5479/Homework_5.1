818c5aa663e299912680a880e74d8b9c7129ca6e
zenin5479 <zenin5479@gmail.com>
2025-07-09 19:29:42 +0300

    Update 2.1 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -16,32 +16,7 @@ namespace Homework_5._1
    {
       static void Main(string[] args)
       {
-         // Обновить методы для int
-         string nameOne = "A";
-         int elementsOne = LibraryFor1DArray.NumberArrayElements(nameOne);
-
-         string pathOne = Path.GetFullPath("a.txt");
-         if (!File.Exists(pathOne))
-         {
-            Console.WriteLine("Ошибка при открытии файла для чтения. Файл не существует");
-         }
-
-         string pathFour = Path.GetFullPath("finish.txt");
-         File.Create(pathFour).Close();
-
-         double[] sourceOne = LibraryFor1DArray.VvodArray(pathOne, nameOne);
-         if (sourceOne.Length == 0)
-         {
-            Console.WriteLine("Исходный строковый массив {0} пуст", nameOne);
-         }
-         else
-         {
-            double[] searchOne = LibraryFor1DArray.InputArray(sourceOne, elementsOne, nameOne);
-            double maxOne = LibraryFor1DArray.FindMaxArray(searchOne, nameOne);
-            double[] replacingOne = LibraryFor1DArray.ReplacingMax(searchOne, maxOne);
-            string[] arrayOne = LibraryFor1DArray.VivodStringArray(replacingOne);
-            LibraryFor1DArray.FileAppendString(arrayOne, pathFour);
-         }
+         
 
          Console.ReadKey();
       }
